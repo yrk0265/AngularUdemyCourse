de99a50482db759d6868b8528ea2c83b585fbbb3
yrk0265 <yrk0265@gmail.com>
2024-04-17 06:04:56 +0530

    1.Modified for Testing Pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,11 @@ import { Course } from './model/course';
 export class AppComponent {
 
     courses = [...COURSES];
+    title=this.courses[0].description;
+    price=9.99;
+    rate=0.67;
+    startDate=new Date(2000,0,1);
+    course = this.courses[0];
     onCourseSelected(course:Course) {
         console.log("App component - click event bubbled....",course);
 
